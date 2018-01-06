127bf48d6bcf0598d2f8c3c3ceb972e9208a3a7a
Amanda Freeman <amandafreeman@Amandas-MacBook-Pro.local>
2018-01-06 02:38:40 -0600

    working portfolio complete

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
- $(".button-collapse").sideNav();
-});
+let introDiv = document.getElementById("intro");
+let projectsDiv = document.getElementById("projects");
+let aboutDiv = document.getElementById("about");
+
+function clickProjects() {
+  projectsDiv.style.display = "block";
+  introDiv.style.display = "none";
+  aboutDiv.style.display = "none";
+};
+
+function clickAbout() {
+  aboutDiv.style.display = "block";
+  introDiv.style.display = "none";
+  projectsDiv.style.display = "none";
+};
